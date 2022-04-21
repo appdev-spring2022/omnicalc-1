f02dfd8b5d7f86e20cfc3efc24c7dbf3654c96fd
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-04-21 04:23:44 +0000

    V3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,33 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num**2
     render({:template => "calculation_templates/square_results.html.erb", })
   end
+
+  def blank_random_form
+    render({:template => "calculation_templates/random_form.html.erb", })
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper).to_f
+
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
+  def blank_payment_form
+    render({:template => "calculation_templates/payment_form.html.erb", })
+  end
+
+  def calculate_payment
+    render({ :template => "calculation_templates/payment_results.html.erb"})
+  end
+
+  def blank_square_root_form
+    render({:template => "calculation_templates/square_root_form.html.erb", })
+  end
+
+  def calculate_square_root
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
 end
-
-
